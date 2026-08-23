cda1e4a2b7fe1a4bc2484ebd8a260a4591d6a509
uartnet <140632163+uartnet@users.noreply.github.com>
2026-08-23 03:28:35 +0200

    chore(deps): refresh build toolchain

--- build-docker-linuxbrew.sh
+++ build-docker-linuxbrew.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 docker buildx build . \
   -f docker/Dockerfile.linuxbrew \
+  --platform linux/amd64 \
   -t "registry.rstream.io/rstream-cpp-linuxbrew:$(cat version.txt)-snapshot-$(date +%F)" \
   -t registry.rstream.io/rstream-cpp-linuxbrew:latest \
   --load \
